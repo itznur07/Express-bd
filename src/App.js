97932f21ba4f3392c9c61f9f407ef42786bdcd3f
nuruddin-99 <nuruddinalways99@gmail.com>
2022-12-30 20:41:31 +0600

    Navbar Component Done!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Navbar from "./Components/Navbar";
 
 function App() {
-  return <div className="App">BD Express Shoping</div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="flex items-center justify-between px-14 py-4 shadow-sm">
+        <div>
+          <h1 className="font-bold text-xl text-[#f57224]">BDExpress</h1>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <i class="fas fa-cart-plus text-slate-900 text-xl"></i>{" "}
+              <span class="bg-[#f57224] px-1 rounded-full mb-3 text-sm text-white">
+                0
+              </span>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
