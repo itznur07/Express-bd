3f1bb37700d6db1d36d6983c2698c3a06c8a8229
Md Nuruddin <nuruddinalways99@gmail.com>
2023-01-03 08:21:59 +0600

    Added router in navbar components

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,20 +1,21 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <>
       <div className="flex items-center justify-between px-14 py-4 shadow-sm shadow-slate-200">
         <div>
-          <h1 className="font-bold text-xl text-[#f57224]">BDExpress</h1>
+          <Link to="/" className="font-bold text-xl text-[#f57224]">BDExpress</Link>
         </div>
         <nav>
           <ul>
-            <li>
+            <Link to='/cart'>
               <i className="fas fa-cart-plus text-slate-900 text-xl"></i>{" "}
               <span className="bg-[#f57224] px-1 rounded-full mb-3 text-sm text-white">
                 0
               </span>
-            </li>
+            </Link>
           </ul>
         </nav>
       </div>
