47888fc09606c00a9e6bfacea2a3ab6eddbf3ad9
Md Nuruddin <nuruddinalways99@gmail.com>
2023-01-06 09:44:51 +0600

    Cart Components Style Done

--- src/Components/Carts.js
+++ src/Components/Carts.js
@@ -7,55 +7,20 @@ const Carts = () => {
 
   return (
     <>
-      <div className='mx-14 mt-8'>
-        <h1 className='text-center text-3xl font-medium space-y-16'>
-          Shopping Cart
-        </h1>
-        <div className='flex place-items-center justify-center'>
-          <div className='flex flex-col'>
-            {shoppingCart.map((product) => (
-              <div key={product.id}>
-                <div className='flex items-center space-x-52 space-y-14'>
-                  {/* Cart img and name section */}
-                  <div className='flex items-center space-x-2'>
-                    <img
-                      className='w-14 rounded-sm'
-                      src={product.image}
-                      alt='cart-img'
-                    />
-                    <h1>{product.name}</h1>
-                  </div>
-
-                  {/* Cart qty section */}
-                  <div className='flex items-center space-x-4 cursor-pointer'>
-                    <button className='px-4 py-1 bg-black text-xl text-white'>
-                      -
-                    </button>
-                    <p>{product.qty}</p>
-                    <button className='px-4 py-1 bg-black text-xl text-white'>
-                      +
-                    </button>
-                  </div>
-
-                  {/* Remove cart button */}
-                  <div>
-                    <button className='text-sm'>❌</button>
-                  </div>
-
-                  {/* Product Price section */}
-                  <div>
-                    <p>${product.price}</p>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-          {/* Total Price Sections */}
-
-          <div className='space-y-6 text-right text-xl font-semibod'>
-            <hr />
-            <h1 className='mr-8'>Total Price: {totalPrice}</h1>
-          </div>
+      <div className='mx-14 mt-24'>
+        <div className='flex flex-col items-center justify-center border border-[#fafafa] rounded-md'>
+          {shoppingCart.length > 0
+            ? shoppingCart.map((product) => <div className="my-5 flex items-center bg-white py-3 px-5" key={product.id}>
+              <span className="inline-block"><img className="cursor-pointer inline w-20 rounded-sm" src={product.image} alt="not-found" /></span>
+              <span className="ml-6 inline-block pr-4">{product.name}</span>
+              <span className="text-center text-[#f57224] mx-6 text-lg">${product.price}</span>
+              <span><i className="fas fa-minus border cursor-pointer  px-2 py-1 border-[#f57224] mx-3 text-sm text-[#f57224] inline-block"></i></span>
+              <span>{product.qty}</span>
+              <span><i className="fas fa-plus border px-2 py-1 border-[#f57224] mx-3 cursor-pointer text-sm text-[#f57224] inline-block"></i></span>
+              <span className="text-[#f57224] mx-6 text-lg block">$500.0</span>
+              <span><i className="fas fa-trash-alt cursor-pointer text-[#f57224] inline-block mx-6 text-lg"></i></span>
+            </div>)
+            : "Your Cart Currently empty"}
         </div>
       </div>
     </>
@@ -63,3 +28,40 @@ const Carts = () => {
 };
 
 export default Carts;
+
+// {shoppingCart.map((product) => (
+//   <div key={product.id}>
+//     <div className='flex items-center space-x-52 space-y-14'>
+//       {/* Cart img and name section */}
+//       <div className='flex items-center space-x-2'>
+//         <img
+//           className='w-14 rounded-sm'
+//           src={product.image}
+//           alt='cart-img'
+//         />
+//         <h1>{product.name}</h1>
+//       </div>
+
+//       {/* Cart qty section */}
+//       <div className='flex items-center space-x-4 cursor-pointer'>
+//         <button className='px-4 py-1 bg-black text-xl text-white'>
+//           -
+//         </button>
+//         <p>{product.qty}</p>
+//         <button className='px-4 py-1 bg-black text-xl text-white'>
+//           +
+//         </button>
+//       </div>
+
+//       {/* Remove cart button */}
+//       <div>
+//         <button className='text-sm'>❌</button>
+//       </div>
+
+//       {/* Product Price section */}
+//       <div>
+//         <p>${product.price}</p>
+//       </div>
+//     </div>
+//   </div>
+// ))}
